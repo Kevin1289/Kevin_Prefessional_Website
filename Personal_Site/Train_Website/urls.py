f70a8df3538c78dd9a366d36bd1d7c680bbd4ebf
Kevin1289 <sxw207@nyu.edu>
2022-01-15 20:43:40 -0500

    add experience and education logos

--- Personal_Site/Train_Website/urls.py
+++ Personal_Site/Train_Website/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import url
-from django.contrib import admin
-from django.urls import include, path
+# from django.conf.urls import url
+from django.urls import include, re_path, path
+# from django.urls import include, path
 from . import views
 from .views import test
 
 urlpatterns = [
-    url(r'^test/$', views.test),
+    re_path(r'^test/$', views.test),
     path('Train_Choose_Service', views.Train_Choose_Service, name='Train_Choose_Service'),
-    url(r'^all_stops/$', views.all_stops, name='all_stops'),
-    url(r'^common_stations/$', views.common_stations, name='common_stations'),
+    re_path(r'^all_stops/$', views.all_stops, name='all_stops'),
+    re_path(r'^common_stations/$', views.common_stations, name='common_stations'),
     path('history', views.History, name='history'),
     path('logout', views.logout_view, name='logout'),
 ]
--- Personal_Site/website/urls.py
+++ Personal_Site/website/urls.py
@@ -1,14 +1,15 @@
-from django.conf.urls import url
+from django.urls import include, re_path, path
 from django.contrib import admin
-from django.urls import include, path
+# from django.conf.urls import url
+# from django.urls import include, path
 
 from Train_Website.views import register, login_view, logout_view
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^register/$', register, name='register'),
-    url(r'^login/$', login_view, name = 'login'),
-    url(r'^logout/$', logout_view),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^register/$', register, name='register'),
+    re_path(r'^login/$', login_view, name = 'login'),
+    re_path(r'^logout/$', logout_view),
     path('', include('resume_page.urls')),
     path('admin/', admin.site.urls),
     path('', include('Train_Website.urls')),
